b43f99ef4f04500b85c1160bd9814abbca940550
Jason Liu <jason@jasonkliu.com>
2014-04-10 22:10:21 -0400

    Current health, harden with pp

--- pokemon/pokemon.rb
+++ pokemon/pokemon.rb
@@ -1,86 +1,3 @@
-class Pokemon
-
-  def name
-    @name
-  end
-
-  def name= newname
-    @name = newname
-  end
-
-  #health getters and setters
-  def current_health
-    @current_health
-  end
-
-  def current_health= newhealth
-    @current_health = newhealth
-  end
-
-  def max_health
-    @max_health
-  end
-
-  def max_health= newhealth
-    @max_health = newhealth
-  end
-
-  def damage amount
-    @current_health = @current_health - amount
-  end
-
-  def fullheal
-    @current_health = @max_health
-  end
-
-  def type
-    @type
-  end
-
-  def type= newtype
-    @type = newtype
-  end
-
-  def type2= newtype
-    if nil != @type     # We have a type, so make a dual-type pokemon
-      @type = @type + '/' + newtype
-    else                # Else, just set it to the regular type.
-      @type = newtype
-    end
-  end
-
-  def cleartype
-    @type = nil
-  end
-
-end
-
-p = Pokemon.new
-p.name= "Bulbasaur"
-
-p.type= "Grass"
-p.type2 = "Poison"
-puts p.type
-
-p.cleartype
-puts p.type
-
-p.type2= "Fairy"
-puts p.type
-
-p.max_health= 24
-p.current_health= 24
-p.damage 4
-p
-p.damage 4
-p
-p.damage 4
-p
-p.damage 4
-p
-
-puts p.current_health
-
 =begin
 There's two options here. 
 class Foo
@@ -99,8 +16,98 @@ print "Instance method: "
 Foo.new.some_instance_method
 =end
 
+class Pokemon
+
+  def name
+    @name
+  end
+  def name= newname
+    @name = newname
+  end
+
+  #health getters and setters
+  def current_health
+    @current_health
+  end
+  def current_health= newhealth
+    @current_health = newhealth
+  end
+
+  def max_health
+    @max_health
+  end
+  def max_health= newhealth
+    @max_health = newhealth
+  end
+
+  def damage amount
+    @current_health = @current_health - amount
+  end
+
+  def fullheal
+    @current_health = @max_health
+  end
+
+  # type - single or dual
+  def type
+    @type
+  end
+  def type= newtype
+    @type = newtype
+  end
+  def type2= newtype
+    if nil != @type     # We have a type, so make a dual-type pokemon
+      @type = @type + '/' + newtype
+    else                # Else, just set it to the regular type.
+      @type = newtype
+    end
+  end
+  def cleartype
+    @type = nil
+  end
+
+  # defense
+  def defense
+    @defense
+  end
+  def defense= ev
+    @defense = ev
+  end
+  def defend amount
+    @defense += amount
+  end
+
+end
+
+p = Pokemon.new
+p.name= "Bulbasaur"
+
+p.type= "Grass"
+p.type2 = "Poison"
+#puts p.type
+
+p.cleartype
+#puts p.type
+
+p.type2= "Fairy"
+#puts p.type
+
+p.max_health= 24
+p.current_health= 24
+p.damage 4
+p
+p.damage 4
+p
+p.damage 4
+p
+p.damage 4
+p
+
+#puts p.current_health
+
 class Kakuna < Pokemon
-   def check
+  
+  def checkpp
     @hardenpp
   end
   def hardenpp
@@ -111,16 +118,25 @@ class Kakuna < Pokemon
     harden its shell to protect itself from predators."
   end
   def harden
-    @hardenpp -= 1
+    if @hardenpp > 0
+      @hardenpp -= 1
+      defend 1
+    else
+      puts "Kakuna used struggle!"
+    end
   end
 end
 
 k = Kakuna.new
 k.pokedex
+k.defense= 50 #won't work in init
 k.hardenpp
-k.harden
-k.harden
-puts k.check
+while k.checkpp > 0 do
+  k.harden
+end
+k.harden # test out the struggle!
+puts "hardenpp " + k.checkpp.to_s
+puts "defense " + k.defense.to_s
 
 
 
@@ -135,3 +151,7 @@ class Pikachu < Pokemon
     #recoverme
   end
 end
+
+pika = Pikachu.new
+pika.thundershock p
+puts p.current_health
