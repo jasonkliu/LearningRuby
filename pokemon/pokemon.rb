d1d12a741e712673e0faca31409eb59cf26a2775
Jason Liu <jason@jasonkliu.com>
2014-04-11 17:01:52 -0400

    Added type functions, condensed to initialize in the class

--- pokemon/pokemon.rb
+++ pokemon/pokemon.rb
@@ -1,32 +1,25 @@
-=begin
-There's two options here. 
-class Foo
-  def self.some_class_method
-    puts self
-  end
-  def some_instance_method
-      self.class.some_class_method
-  end
-end
-
-print "Class method: "
-Foo.some_class_method
-
-print "Instance method: "
-Foo.new.some_instance_method
-=end
+# We have a main Pokemon class, which allows you to 'init', setting values
+# of all of the different stats, and also damage, etc. Each separate Pokemon
+# inherits from the Pokemon class, which allows it to use the same methods.
 
 class Pokemon
   # https://stackoverflow.com/questions/4370960/what-is-attr-accessor-in-ruby
   # instead of def name @name end def name= newname @name = newname etc
-  attr_accessor :name, :max_health, :current_health, :attack, :defense 
+  attr_accessor :name, :max_health, :current_health, :attack, :defense, :type
   attr_reader :cry, :moves
 
-  def init(name = "Missingno", max_health = 169, attack = 10, defense = 10, cry = "BWAAAA")
+  def init(name = "Missingno", max_health = 169, attack = 10,
+   defense = 10, cry = "BWAAAA", type = "Grass", type2 = "Poison")
     @name = name
     @cry = cry
     @attack = attack
     @defense = defense
+
+    if nil != type && nil != type2   # Make a dual-type pokemon
+      @type = type + '/' + type2
+    else                             # Set it to the regular type.
+      @type = type
+    end
     
     if max_health < 10 || max_health > 999  # Over our boundaries.
       @max_health = 169
@@ -35,7 +28,8 @@ class Pokemon
     end
     @current_health = @max_health
 
-    puts @max_health.to_s
+    #puts @type
+    #puts @max_health.to_s
     puts @cry  # The Pokemon cries out!
   end
 
@@ -47,24 +41,6 @@ class Pokemon
     @current_health = @max_health
   end
 
-  # type - single or dual
-  def type
-    @type
-  end
-  def type= newtype
-    @type = newtype
-  end
-  def type2= newtype
-    if nil != @type     # We have a type, so make a dual-type pokemon
-      @type = @type + '/' + newtype
-    else                # Else, just set it to the regular type.
-      @type = newtype
-    end
-  end
-  def cleartype
-    @type = nil
-  end
-
   # defense
   def boost_defense amount
     @defense += amount
@@ -72,44 +48,21 @@ class Pokemon
 
 end
 
-p = Pokemon.new
-p.init
-p.name= "Bulbasaur"
-
-p.type= "Grass"
-p.type2 = "Poison"
-#puts p.type
-
-p.cleartype
-#puts p.type
-
-p.type2= "Fairy"
-#puts p.type
-
-p.max_health= 24
-p.current_health= 24
-p.damage 4
-p
-p.damage 4
-p
-p.damage 4
-p
-p.damage 4
-p
-
-#puts p.current_health
-
 class Kakuna < Pokemon
-  
+  def initialize(name = self.class.name, cry = "waaaaaah!")
+    self.init(name, 130, 10, 10, cry, "Bug", "Poison")
+  end
+
+  def pokedex  
+    puts "    Almost incapable of moving, this Pokémon can only 
+    harden its shell to protect itself from predators."
+  end
+
   def checkpp
     @hardenpp
   end
   def hardenpp
     @hardenpp = 30;
-  end
-  def pokedex  
-    puts "    Almost incapable of moving, this Pokémon can only 
-    harden its shell to protect itself from predators."
   end
   def harden
     if @hardenpp > 0
@@ -122,8 +75,8 @@ class Kakuna < Pokemon
 end
 
 k = Kakuna.new
+puts k.max_health.to_s
 k.pokedex
-k.defense= 50 #won't work in init
 k.hardenpp
 while k.checkpp > 0 do
   k.harden
@@ -131,8 +84,6 @@ end
 k.harden # test out the struggle!
 puts "hardenpp " + k.checkpp.to_s
 puts "defense " + k.defense.to_s
-
-
 
 class Pikachu < Pokemon
   def thundershock otherpokemon
@@ -146,6 +97,6 @@ class Pikachu < Pokemon
   end
 end
 
-pika = Pikachu.new
-pika.thundershock p
-puts p.current_health
+#pika = Pikachu.new
+#pika.thundershock p
+#puts p.current_health
